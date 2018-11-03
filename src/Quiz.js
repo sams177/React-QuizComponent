1d64c5c075bf1cf0d1b89a9ee8f196af68aa807e
sams177 <sams177@gmail.com>
2018-11-02 20:00:45 -0400

    completed first module

--- src/Quiz.js
+++ src/Quiz.js
@@ -7,9 +7,7 @@ class Quiz extends Component{
     constructor(props){
         super(props);
 
-        this.setState({
-            quiz_position: 1
-        });
+        this.setState = {quiz_position: 1};
     }
     render(){
         return(
